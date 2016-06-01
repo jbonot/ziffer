e8c16871cad301c49910bc2fbf11c85cf252ea06
Jeanine Bonot <jeanine.bonot@rwth-aachen.de>
2016-06-01 20:25:04 +0200

    Create activity for viewing list of created events.

---
+++ app/src/main/java/de/rwth_aachen/ziffer/MyCreatedEvents.java
@@ -0,0 +1,39 @@
+package de.rwth_aachen.ziffer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class MyCreatedEvents extends AppCompatActivity {
+
+    private ListAdapter listAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_created_events);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MyCreatedEvents.this, EventCreate.class);
+                startActivity(intent);
+            }
+        });
+
+        // Add sample data to event list.
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listAdapter = TestData.getEventListAdapter(this);
+        listView.setAdapter(listAdapter);
+    }
+
+}
