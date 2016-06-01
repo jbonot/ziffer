7ffb0e143d81568508450e6df13b33b9d6803d12
Jeanine Bonot <jeanine.bonot@rwth-aachen.de>
2016-06-01 20:41:41 +0200

    Create activity to display joined events.

---
+++ app/src/main/java/de/rwth_aachen/ziffer/MyJoinedEvents.java
@@ -0,0 +1,25 @@
+package de.rwth_aachen.ziffer;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+
+public class MyJoinedEvents extends AppCompatActivity {
+
+    private ListAdapter listAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.listview);
+
+        // Add sample data to event list.
+        ListView listView = (ListView) findViewById(R.id.listView);
+        listAdapter = TestData.getEventListAdapter(this);
+        listView.setAdapter(listAdapter);
+    }
+}
