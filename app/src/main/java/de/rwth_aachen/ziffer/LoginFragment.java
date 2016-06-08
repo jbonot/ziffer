e17e9a34de3d9478a0f71e311b9fac8331fe9999
Jeanine Bonot <jeanine.bonot@rwth-aachen.de>
2016-06-08 13:42:50 +0200

    Go immediately to home page if already logged in.

--- app/src/main/java/de/rwth_aachen/ziffer/LoginFragment.java
+++ app/src/main/java/de/rwth_aachen/ziffer/LoginFragment.java
@@ -93,8 +93,10 @@ public class LoginFragment extends Fragment  {
     @Override
     public void onResume() {
         super.onResume();
-        Profile profile = Profile.getCurrentProfile();
-        mTextDetails.setText(constructWelcomeMessage(profile));
+        
+        // Go immediately to the home page.
+        Intent intent = new Intent(getActivity(), MainActivity.class);
+        startActivity(intent);
     }
 
     @Override
