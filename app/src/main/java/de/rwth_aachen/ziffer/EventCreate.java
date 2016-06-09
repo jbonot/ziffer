5ebb6d3044d26f915efb43b0fcab611c90f69a99
Jeanine Bonot <jeanine.bonot@rwth-aachen.de>
2016-06-09 20:20:39 +0200

    Move "save" and "cancel" buttons in create event form to toolbar.

--- app/src/main/java/de/rwth_aachen/ziffer/EventCreate.java
+++ app/src/main/java/de/rwth_aachen/ziffer/EventCreate.java
@@ -1,8 +1,11 @@
 package de.rwth_aachen.ziffer;
 
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -13,12 +16,15 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-public class EventCreate extends FragmentActivity {
+public class EventCreate extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_create_form);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
         Calendar c = Calendar.getInstance();
 
         TextView timeStart = (TextView)findViewById(R.id.timeStart);
@@ -92,6 +98,25 @@ public class EventCreate extends FragmentActivity {
 
         this.setupSpinner((Spinner) findViewById(R.id.maxAttendees), categories);
         this.setupSpinner((Spinner) findViewById(R.id.minAttendees), categories);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.savecancel, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_save) {
+            return true;
+        } else if (id == R.id.action_cancel) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private void setupSpinner(Spinner spinner, List<String> values) {
