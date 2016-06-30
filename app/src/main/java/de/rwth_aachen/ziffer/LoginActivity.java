b74446b67f9dd1d367ff812bd44e918e2ff060e1
Jeanine Bonot <jeanine.bonot@rwth-aachen.de>
2016-06-30 18:29:56 +0200

    Navigate to the main activity after successfully logging in.

--- app/src/main/java/de/rwth_aachen/ziffer/LoginActivity.java
+++ app/src/main/java/de/rwth_aachen/ziffer/LoginActivity.java
@@ -3,6 +3,7 @@ package de.rwth_aachen.ziffer;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -333,6 +334,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                LoginActivity.this.startActivity(new Intent(LoginActivity.this, MainActivity.class));
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
