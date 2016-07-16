1a1e9c8d20186560cc9fdb078d80ed9a8816ff03
jasimwhd <jasimwhd@gmail.com>
2016-07-16 22:46:59 +0200

    updated files , check db_nam

--- sql/myevent.php
+++ sql/myevent.php
@@ -1,10 +1,11 @@
 <?php
-$db_name="webappdb";
+$db_name="webappdb_new";
 $mysql_user="root";
 $mysql_pass="admin";
 $server_name="localhost";
-$user_name_host = $_POST["user_name"];  
-  
+$user_name_host=$_POST["user_name"];
+
+
 // Create connection
 $con = mysqli_connect($server_name,$mysql_user,$mysql_pass,$db_name);
 // Check connection
@@ -12,38 +13,66 @@ if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 } 
 
-$sql = "SELECT * FROM `event` WHERE `user_name_host`='".$user_name_host."' ;";
+$sql = "SELECT e.german_level_event,
+e.title,
+e.date,
+e.start_time,
+e.end_time,
+e.min_attendees,
+e.max_attendees,
+e.description,
+l.name,
+l.address
+FROM `event` AS e
+JOIN 
+`event_locations` AS l
+ON
+e.user_name_host='".$user_name_host."'
+AND
+e.location_id=l.location_id ;"; 
+
 $result = mysqli_query($con, $sql);
- 
+
 $response = array();
  
 while($row = mysqli_fetch_array($result)){
-/*   $response = array("event_id"=>$row[0],
-   "user_name_host"=>$row[1],
-   "german_level_event"=>$row[2],
-   "title"=>$row[3],
-   "location"=>$row[4],
-   "date"=>$row[5],
-   "start_time"=>$row[6],
-   "end_time"=>$row[7],
-   "min_attendees"=>$row[8],
-   "max_attendees"=>$row[9],
-   "description"=>$row[10]); */
-   array_push($response,array('event_id'=>$row[0],
-   'user_name_host'=>$row[1],
-   'german_level_event'=>$row[2],
-   'title'=>$row[3],
-   'location'=>$row[4],
-   'date'=>$row[5],
-   'start_time'=>$row[6],
-   'end_time'=>$row[7],
-   'min_attendees'=>$row[8],
-   'max_attendees'=>$row[9],
-   'description'=>$row[10]));
+/* echo $row[0]."<br>";
+ echo $row[1]."<br>";
+ echo $row[2]."<br>";
+ echo $row[3]."<br>";
+ echo $row[4]."<br>";
+ echo $row[5]."<br>";
+ echo $row[6]."<br>";
+ echo $row[7]."<br>";
+ echo $row[8]."<br>";
+ echo $row[9]."<br>";
+ */
+ 
+ array_push($response,array(
+   'german_level_event' => $row[0],
+   'title' => $row[1],
+   'date' => $row[2],
+   'start_time' => $row[3],
+   'end_time' => $row[4],
+  'min_attendees' => $row[5],
+   'max_attendees' => $row[6],
+  'description' => $row[7],
+   'name' => $row[8],
+   'address' => $row[9]));
    
-//  $response = array("user_info.user_info_id"=>$row[0],"user_info.user_name"=>$row[1],"user_info.user_pass"=>$row[2],"profile_data.firstName"=>$row[5],"profile_data.lastName"=>$row[6],"profile_data.dob"=>$row[7],"profile_data.german_level"=>$row[8],"profile_data.description"=>$row[9]);
 }
 
-echo json_encode(array("event_data"=>$response));
+function utf8ize($d) {
+    if (is_array($d)) {
+        foreach ($d as $k => $v) {
+            $d[$k] = utf8ize($v);
+        }
+    } else if (is_string ($d)) {
+        return utf8_encode($d);
+    }
+    return $d;
+}
+
+echo json_encode(array("event_data"=>utf8ize($response)));
  
 ?>
--- sql/myprofile.php
+++ sql/myprofile.php
@@ -1,9 +1,13 @@
 <?php
-$db_name="webappdb";
+$db_name="webappdb_new";
 $mysql_user="root";
 $mysql_pass="admin";
 $server_name="localhost";
-$user_name = $_POST["user_name"];  
+//$user_name = $_POST["user_name"];
+$user_name = "jasimwhd";
+
+
+  
   
 // Create connection
 $con = mysqli_connect($server_name,$mysql_user,$mysql_pass,$db_name);
@@ -18,8 +22,15 @@ $result = mysqli_query($con, $sql);
 $response = array();
  
 while($row = mysqli_fetch_array($result)){
-   $response = array("profile_data_id"=>$row[0],"user_name"=>$row[1],"firstName"=>$row[2],"lastName"=>$row[3],"dob"=>$row[4],"german_level"=>$row[5],"description"=>$row[6]);
-//  $response = array("user_info.user_info_id"=>$row[0],"user_info.user_name"=>$row[1],"user_info.user_pass"=>$row[2],"profile_data.firstName"=>$row[5],"profile_data.lastName"=>$row[6],"profile_data.dob"=>$row[7],"profile_data.german_level"=>$row[8],"profile_data.description"=>$row[9]);
+   $response = array("profile_data_id"=>$row[0],
+   "user_name"=>$row[1],
+   "firstName"=>$row[2],
+   "lastName"=>$row[3],
+   "dob"=>$row[4],
+   "german_level"=>$row[5],
+   "description"=>$row[6],
+   "image"=>$row[8]);
+
 }
 
 echo json_encode(array("profile_data"=>$response));
