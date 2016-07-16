df364bd4854b0ab42bb04faa2121338fec125ece
jasimwhd <jasimwhd@gmail.com>
2016-07-16 22:20:04 +0200

    profile info updated

--- app/src/main/java/de/rwth_aachen/ziffer/EventDetails.java
+++ app/src/main/java/de/rwth_aachen/ziffer/EventDetails.java
@@ -4,15 +4,23 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class EventDetails extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Bundle b = getIntent().getExtras();
+        String event_data = b.getString("event_data");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_details);
+
         setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+
+
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         findViewById(R.id.hostIcon).setOnClickListener(new View.OnClickListener() {
