eb783a9809ef8b579b3272e94d487ccc2e379a77
Jeanine Bonot <jeanine.bonot@rwth-aachen.de>
2016-07-17 13:12:18 +0200

    Fetch notifications from the database.

--- app/src/main/java/de/rwth_aachen/ziffer/NotificationsFragment.java
+++ app/src/main/java/de/rwth_aachen/ziffer/NotificationsFragment.java
@@ -2,6 +2,7 @@ package de.rwth_aachen.ziffer;
 
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -10,6 +11,21 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 /**
@@ -29,8 +45,33 @@ public class NotificationsFragment extends Fragment {
     public void onViewCreated(View view, Bundle savedInstanceState) {
         // Add sample data to event list.
         ListView listView = (ListView)view.findViewById(R.id.listView);
-        listAdapter = new TestData(notification_data).getNotificationListAdapter(getActivity());
-        listView.setAdapter(listAdapter);
+        BackgroundTask task = new BackgroundTask();
+        task.execute("get_notifications", "andrea.allen");
+        try {
+            String data = task.get();
+            List<NotificationListItem> list = new ArrayList<>();
+            JSONArray arr = new JSONObject(data).getJSONArray("notifications");
+            for (int i = 0; i < arr.length(); i++) {
+                JSONObject notification = new JSONObject(arr.getString(i));
+                notification.getInt("notification_id");
+                notification.getInt("event_id");
+                notification.getInt("read_status");
+                notification.getString("user_sender");
+                String formatString = getResources().getStringArray(R.array.notifications_array)[notification.getInt("message_type")];
+                String senderName = notification.getString("sender_firstname") + " " + notification.getString("sender_lastname");
+                list.add(new NotificationListItem(String.format(formatString, senderName, notification.getString("event_name")), notification.getString("timestamp")));
+            }
+
+            listAdapter = new NotificationListAdapter(getActivity(),list.toArray(new NotificationListItem[0]));
+            listView.setAdapter(listAdapter);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -40,4 +81,39 @@ public class NotificationsFragment extends Fragment {
         });
     }
 
+    private class BackgroundTask extends AsyncTask<String,Void,String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            if (params[0].equals("get_notifications")) {
+                try
+                {
+                    String urlParams = "user_recipient=" + URLEncoder.encode(params[1], "UTF-8");
+                    HttpURLConnection httpURLConnection = (HttpURLConnection) new URL(LocalSettings.Base_URL + "get_notifications.php").openConnection();
+                    httpURLConnection.setDoOutput(true);
+                    OutputStream os = httpURLConnection.getOutputStream();
+                    os.write(urlParams.getBytes());
+                    os.flush();
+                    os.close();
+
+                    int tmp;
+                    String data = "";
+                    InputStream is = httpURLConnection.getInputStream();
+                    while((tmp = is.read())!= -1){
+                        data += (char)tmp;
+                    }
+
+                    is.close();
+                    httpURLConnection.disconnect();
+                    return data;
+                }
+                catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+    }
 }
---
+++ sql/get_notifications.php
@@ -0,0 +1,59 @@
+<?php
+$db_name="webappdb";
+$mysql_user="root";
+$mysql_pass="";
+$server_name="localhost";
+
+$user_recipient = !empty($_POST['user_recipient']) ? $_POST['user_recipient'] : '';
+
+// Create connection
+$con = mysqli_connect($server_name,$mysql_user,$mysql_pass,$db_name);
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $con->connect_error);
+} 
+
+$sql = "SELECT n.notifications_id, n.event_id, n.event_name,
+		n.message_type, n.timestamp, n.read_status,
+		n.user_sender, profile_data.firstName, profile_data.lastName
+		FROM profile_data JOIN
+			(SELECT notifications.notifications_id, notifications.event_id,
+			notifications.message_type, notifications.timestamp,
+			notifications.read_status, notifications.user_sender,
+			event.title as event_name
+			FROM notifications JOIN event
+			ON event.event_id = notifications.event_id
+			WHERE user_recipient='$user_recipient') AS n
+		ON n.user_sender = profile_data.user_name;";
+
+
+$result = mysqli_query($con, $sql);
+
+$response = array();
+while($row = mysqli_fetch_array($result)){
+   array_push($response,array(
+   'notification_id' => $row[0],
+   'event_id' => $row[1],
+   'event_name' => $row[2],
+   'message_type' => $row[3],
+   'timestamp' => $row[4],
+   'read_status' => $row[5],
+   'user_sender' => $row[6],
+   'sender_firstname' => $row[7],
+   'sender_lastname' => $row[8]));
+}
+
+function utf8ize($d) {
+    if (is_array($d)) {
+        foreach ($d as $k => $v) {
+            $d[$k] = utf8ize($v);
+        }
+    } else if (is_string ($d)) {
+        return utf8_encode($d);
+    }
+    return $d;
+}
+
+echo json_encode(array("notifications"=>utf8ize($response)));
+
+?>
